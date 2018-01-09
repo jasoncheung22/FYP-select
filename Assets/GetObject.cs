d81b5f258bad97d1c99ac17cb0ee297da02ee8a9
jasoncheung22 <31604325+jasoncheung22@users.noreply.github.com>
2018-01-09 12:52:06 +0800

    first

---
+++ Assets/GetObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GetObject : MonoBehaviour{
+
+    public float SelectedTime =0f;
+    public static ArrayList SelectedFoodList = new ArrayList();
+    private GetObject GetTemp;
+    // Use this for initialization
+    void Start () {
+        GetTemp = GetComponent<GetObject>();
+
+    }
+
+    private void OnDisable()
+    {
+        SelectedTime = 0f;
+    }
+
+    // Update is called once per frame
+    public void Update () {
+
+        SelectedTime += Time.deltaTime;
+
+        if (SelectedTime >= 3f)
+        {
+            GetTemp.enabled = false;
+            string temp = FoodManager.PassObjcetName();
+            Debug.Log("The selection is :"+ temp);
+            if(temp == "remove")
+            {
+                RemoveFood();
+            }
+            else if (temp == "submit")
+            {
+                ShowFood();
+            }
+            else
+            {
+                PutFood(temp);
+            }
+        }
+	}
+    public static void PutFood(string SelectedFood) 
+    {
+        Debug.Log("The selection putting in to the list :" + SelectedFood);
+        SelectedFoodList.Add(SelectedFood);
+    }
+
+    public static void RemoveFood()
+    {
+        SelectedFoodList.RemoveAt(SelectedFoodList.Count - 1);
+    }
+
+    public static void ShowFood()
+    {
+        foreach(var item in SelectedFoodList)
+        {
+            Debug.Log("Item in the list:" + item);
+        }
+        SelectedFoodList.Clear();
+    }
+
+}
---
+++ Assets/Script/FoodManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodManager : MonoBehaviour {
+    Color OrigCol;
+    public static string ObjectName;
+    // Use this for initialization
+
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void GetTheFood()
+    {
+        OrigCol = gameObject.GetComponent<Renderer>().material.color;
+        ObjectName = this.gameObject.name;
+    }
+
+    public static string PassObjcetName()
+    {
+        return ObjectName;
+    }
+
+
+    public void LeaveTheFood()
+    {
+        gameObject.GetComponent<Renderer>().material.color = OrigCol;
+        ObjectName = null;
+    }
+}
